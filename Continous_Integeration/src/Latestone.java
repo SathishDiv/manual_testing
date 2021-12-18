cf4a2266e676818e2472f13261fb791b6296e371
sathishsuriya <sathishmd.mailme@gmail.com>
2021-12-18 13:19:45 +0530

    last one

---
+++ Continous_Integeration/src/Latestone.java
@@ -0,0 +1,11 @@
+
+public class Latestone {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("i love nayanthaara");
+System.out.println("i love trisha");
+System.out.println("i love samantha");
+	}
+
+}
